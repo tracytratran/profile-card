3300806d53505af5bec382fa2709b61a74aaed59
Tracy Tran <tratran.315@gmail.com>
2024-10-30 13:35:37 +0100

    Upload profile-card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,110 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const tracy = {
+  photoName: "/IMG_3171a.jpeg",
+  name: "Tracy Tran",
+  intro:
+    "I'm Tracy, a budding Software Developer with a passion for problem-solving. I'm eager to learn, both solution-oriented and detail-oriented, and dedicated to my craft. While my knowledge in front-end development is limited, I'm committed to expanding my skills and creating user-friendly applications.",
+  // skillList: [
+  //   { name: "JavaScript", color: "#fff933", emoji: "💪" },
+  //   { name: "HTML + CSS", color: "#ff7733", emoji: "💪" },
+  //   { name: "Ionic + Angular", color: "#ff4233", emoji: "👶" },
+  //   { name: "React", color: "#33d7ff", emoji: "💪" },
+  // ],
+};
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "beginner",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "beginner",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "beginner",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#60DAFB",
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList name="JavaScript" />
+      </div>
+    </div>
+  );
+}
+function Avatar() {
+  return <img className="avatar" src={tracy.photoName} alt={tracy.name} />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>{tracy.name}</h1>
+      <p>{tracy.intro}</p>
+    </div>
+  );
+}
+
+function SkillList(props) {
+  return (
+    <div className="skill-list">
+      {/* {tracy.skillList.map((skill) => (
+        <Skill skillObject={skill} key={skill.name} />
+      ))} */}
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} level={skill.level} color={skill.color} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill, level, color }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      {/* <span>{`${
+        level === "beginner"
+          ? "👶"
+          : level === "intermediate"
+          ? "👍"
+          : level === "advanced"
+          ? "💪"
+          : ""
+      }`}</span> */}
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
